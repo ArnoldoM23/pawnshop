37d12a6b53e3c0a69c1ef453800eb5692b56516b
AnorldoM23 <arnoldomunoz23@yahoo.com>
2017-01-04 21:36:14 -0800

    mailCtrl

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,8 +3,10 @@ import { browserHistory } from 'react-router';
 
 
 export function sendEmail(message){
-	return function(){
-		console.log('in side the actions ', message);
-	}
-}
+	return function(dispatch){
+		axios.post('/sendEmail', message)
+			.then(response => console.log('in side the actions ', message))
+			.catch(err => console.log('Error: ', err));	
+	};
+};
 
